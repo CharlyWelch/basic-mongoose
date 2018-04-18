903fc860049c3776ceb50cc7392adbecbdb71f1a
CharlyWelch <blwbiology@gmail.com>
2018-04-17 17:36:27 -0700

    add validation test for enum habitat

--- test/unit/Animal.test.js
+++ test/unit/Animal.test.js
@@ -18,5 +18,33 @@ describe('Animal model', () => {
 
         assert.deepEqual(animal.toJSON(), { _id: animal._id, ...data });
 
+        assert.isUndefined(animal.validateSync());
+    });
+
+    const getValidationErrors = validation => {
+        assert.isDefined(validation, 'expected to encounter errors but got none');
+        return validation.errors;
+    };
+
+    it('requires required fields', () => {
+        const animal = new Animal({});
+        const errors = getValidationErrors(animal.validateSync());
+        assert.equal(errors.common.kind, 'required');
+        assert.equal(errors['binomial.genus'].kind, 'required');
+        assert.equal(errors.habitat.kind, 'required');
+    });
+
+    it('habitat must be enum', () => {
+        const animal = new Animal({
+            common: 'Trash Panda',
+            binomial: {
+                genus: 'Procyon',
+                species: 'lotor'
+            },
+            habitat: 'garbage can',
+            features: ['striped tail', 'mischievous eyes', 'bad attitude']
+        });
+        const errors = getValidationErrors(animal.validateSync());
+        assert.equal(errors['habitat'].kind, 'enum');
     });
 });
