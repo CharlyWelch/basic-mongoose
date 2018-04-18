efde1c88026deacadca200dfa70b8aec54c55999
CharlyWelch <blwbiology@gmail.com>
2018-04-18 09:03:26 -0700

    add test for saving and getting an animal

--- test/e2e/animals.test.js
+++ test/e2e/animals.test.js
@@ -6,5 +6,31 @@ const { dropCollection } = require('./db');
 describe('Animals API', () => {
     before(() => dropCollection('animals'));
 
+    const squid = {
+        common: 'Giant Squid',
+        binomial: {
+            genus: 'Architeuthis',
+            species: 'dux',
+        },
+        habitat: 'marine',
+        features: ['tentacles', 'beak', 'siphon']
+    };
+    const sloth = {
+        common: 'Three Toed Sloth',
+        binomial: {
+            genus: 'Bradypus',
+        },
+        habitat: 'terrestrial',
+        features: ['slow', 'cute', 'nine cervical vertebrae']
+    };
 
+    it('saves and gets an animal', () => {
+        return new Animal(squid).save()
+            .then(saved => {
+                saved = saved.toJSON();
+                const { _id, __v, common } = saved;
+                assert.ok(_id);
+                assert.equal(__v, 0);
+            });
+    });
 });
